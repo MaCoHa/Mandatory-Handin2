683f5ad3576379f8a5639a9e09a46d8be239ef62
coha <coha@itu.dk>
2022-10-12 16:47:21 +0200

    Bob now respons and sends the structs as byte[]

--- Bob/main.go
+++ Bob/main.go
@@ -6,6 +6,7 @@ import (
 	"context"
 	"crypto/ecdsa"
 	"crypto/x509"
+	"encoding/json"
 	"fmt"
 	"math/rand"
 	"net"
@@ -18,10 +19,26 @@ const (
 	port = ":6060"
 )
 
+type Commitment struct {
+	commitmentHash string
+	signature      []byte
+}
+type Reply struct {
+	message   string
+	signature []byte
+}
+type Control struct {
+	random    string
+	message   string
+	signature []byte
+}
+
 var BobsPrivateSignKey *ecdsa.PrivateKey
 var AlicePublicSignKey *ecdsa.PublicKey
 
-var commitment string
+var commitment Commitment
+var reply Reply
+var control Control
 var BobsMessage int
 
 type BobsDiceServer struct {
@@ -47,31 +64,57 @@ func main() {
 
 }
 
-func (s *BobsDiceServer) SendCommitment(ctx context.Context, rec *pb.HashMessage) (*pb.Reply, error) {
-	commitment = rec.Commitment
-	var BobsMessage = rand.Intn(100)
+func (s *BobsDiceServer) SendCommitment(ctx context.Context, rec *pb.CommitmentMessage) (*pb.Reply, error) {
+	err := json.Unmarshal([]byte(rec.CipherMessage), commitment)
+	if err != nil {
+		panic(err)
+	}
+	// Check if the message is from Alice
+	if enc.Valid(AlicePublicSignKey, commitment.commitmentHash, commitment.signature) {
+		var BobsMessage = rand.Intn(100)
+		var sign = enc.Sign(BobsPrivateSignKey, strconv.Itoa(BobsMessage))
+		reply.signature = sign
+		reply.message = strconv.Itoa(BobsMessage)
 
-	resp := &pb.Reply{Message: strconv.Itoa(BobsMessage)}
-	return resp, nil
+		json, err := json.Marshal(reply)
+		if err != nil {
+			panic(err)
+		}
+		resp := &pb.Reply{CipherMessage: json}
+		return resp, nil
+	} else {
+		resp := &pb.Reply{CipherMessage: nil}
+		return resp, nil
+	}
+
 }
 
 func (s *BobsDiceServer) SendMessage(ctx context.Context, rec *pb.ControlMessage) (*pb.Void, error) {
 
-	var hash = enc.GetHash(rec.Message, rec.Random)
-	fmt.Printf("with the message :: %s and with random %s\n", rec.Message, rec.Random)
-	fmt.Printf("Bob generates a hash that is :: %s\n", hash)
-
-	if hash == commitment {
-		fmt.Printf("Alice sent the same message as was in here commitment\n")
-		intVar, _ := strconv.Atoi(rec.Message)
-		dice := (BobsMessage * intVar % 6)
-		fmt.Printf("bob now calculates the dice to :: %d\n", dice)
-	} else {
-		fmt.Printf("The commitment Alice sent does not match with what she sent.\n")
-		fmt.Printf("commitment =          %s\n", commitment)
-		fmt.Printf("Bobs generated hash = %s\n", hash)
+	err := json.Unmarshal([]byte(rec.CipherMessage), control)
+	if err != nil {
+		panic(err)
 	}
+	// Check if the message is from Alice
+	if enc.Valid(AlicePublicSignKey, control.message, control.signature) {
+		//message is from Alice and we see if she sent the correct message and random
+		var hash = enc.GetHash(control.message, control.random)
 
+		fmt.Printf("with the message :: %s and with random %s\n", control.message, control.random)
+		fmt.Printf("Bob generates a hash that is :: %s\n", hash)
+
+		if hash == commitment.commitmentHash {
+			fmt.Printf("Alice sent the same message as was in here commitment\n")
+			intVar, _ := strconv.Atoi(control.message)
+			dice := (BobsMessage * intVar % 6)
+			fmt.Printf("bob now calculates the dice to :: %d\n", dice)
+		} else {
+			fmt.Printf("The commitment Alice sent does not match with what she sent.\n")
+			fmt.Printf("commitment =          %s\n", commitment.commitmentHash)
+			fmt.Printf("Bobs generated hash = %s\n", hash)
+		}
+	}
+	//grpc allways need a return so no matter what bob responds with an empty message
 	resp := &pb.Void{}
 	return resp, nil
 }
